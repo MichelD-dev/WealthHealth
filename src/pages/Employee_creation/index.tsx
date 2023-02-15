84fb7f1b2351e6f1e721120793bb2b6ebd327579
michelD-dev <delaunaymichel@hotmail.fr>
2023-02-15 15:08:54 +0100

    Logique validation formulaire

--- src/pages/Employee_creation/index.tsx
+++ src/pages/Employee_creation/index.tsx
@@ -1,4 +1,37 @@
+import {SubmitHandler, useForm} from 'react-hook-form'
+import {zodResolver} from '@hookform/resolvers/zod'
+import {EmployeeSchema, EmployeeSchemaType} from '@/types/employee.model'
+import {useEffect} from 'react'
+
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setFocus,
+    reset,
+    formState,
+    formState: {errors, isSubmitting, isSubmitSuccessful},
+  } = useForm<EmployeeSchemaType>({
+    resolver: zodResolver(EmployeeSchema),
+  })
+
+  useEffect(() => {
+    setFocus('firstname')
+  }, [setFocus])
+
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset()
+    }
+  }, [formState, reset])
+
+  const onSubmit: SubmitHandler<EmployeeSchemaType> = data => {
+    console.log(data)
+  }
+
+  const address = watch('address')
+
   return (
     <section>
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -7,7 +40,11 @@ const Form = () => {
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
               Create Employee
             </h1>
-            <form className="space-y-4 md:space-y-6">
+            <form
+              className="space-y-4 md:space-y-6"
+              onSubmit={handleSubmit(onSubmit)}
+              noValidate
+            >
               <div>
                 <label
                   htmlFor="firstname"
@@ -18,8 +55,16 @@ const Form = () => {
                 <input
                   type="text"
                   id="firstname"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                  className={`bg-gray-50 border  text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 focus:outline-none ${
+                    errors.firstname ? 'border-red-500' : 'border-gray-300'
+                  }`}
+                  {...register('firstname')}
                 />
+                {errors.firstname && (
+                  <span className="text-red-600 block mt-2 text-right">
+                    {errors.firstname?.message}
+                  </span>
+                )}
               </div>
               <div>
                 <label
@@ -31,8 +76,16 @@ const Form = () => {
                 <input
                   type="text"
                   id="lastname"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                  className={`bg-gray-50 border  text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 focus:outline-none ${
+                    errors.lastname ? 'border-red-500' : 'border-gray-300'
+                  }`}
+                  {...register('lastname')}
                 />
+                {errors.lastname && (
+                  <span className="text-red-600 block mt-2 text-right">
+                    {errors.lastname?.message}
+                  </span>
+                )}
               </div>
               <div>
                 <label
@@ -44,8 +97,16 @@ const Form = () => {
                 <input
                   type="email"
                   id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                  className={`bg-gray-50 border  text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 focus:outline-none ${
+                    errors.email ? 'border-red-500' : 'border-gray-300'
+                  }`}
+                  {...register('email')}
                 />
+                {errors.email && (
+                  <span className="text-red-600 block mt-2 text-right">
+                    {errors.email?.message}
+                  </span>
+                )}
               </div>
               <div>
                 <label
@@ -57,8 +118,16 @@ const Form = () => {
                 <input
                   type="date"
                   id="date"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-                />
+                  className={`bg-gray-50 border  text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 focus:outline-none ${
+                    errors.birthdate ? 'border-red-500' : 'border-gray-300'
+                  }`}
+                  {...register('birthdate')}
+                />{' '}
+                {errors.birthdate && (
+                  <span className="text-red-600 block mt-2 text-right">
+                    {errors.birthdate?.message}
+                  </span>
+                )}
               </div>
               <div>
                 <label
@@ -70,70 +139,116 @@ const Form = () => {
                 <input
                   type="date"
                   id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                  className={`bg-gray-50 border  text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 focus:outline-none ${
+                    errors.startdate ? 'border-red-500' : 'border-gray-300'
+                  }`}
+                  {...register('startdate')}
                 />
+                {errors.startdate && (
+                  <span className="text-red-600 block mt-2 text-right">
+                    {errors.startdate?.message}
+                  </span>
+                )}
               </div>
-              <fieldset className="border border-solid border-gray-300 p-3">
-                <legend className="text-sm">Address</legend>
-                <div>
-                  <label
-                    htmlFor="street"
-                    className="block mb-2 text-sm font-medium text-gray-900 "
-                  >
-                    Street
-                  </label>
-                  <input
-                    type="text"
-                    id="street"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="city"
-                    className="block mb-2 text-sm font-medium text-gray-900 "
-                  >
-                    City
-                  </label>
-                  <input
-                    type="text"
-                    id="city"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="state"
-                    className="block mb-2 text-sm font-medium text-gray-900 "
-                  >
-                    State
-                  </label>
 
-                  <div className="mb-3 xl:w-96">
-                    <select
-                      id="state"
-                      className="form-select bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 "
-                    >
-                      <option value="1">Alabama</option>
-                      <option value="2">Ohio</option>
-                      <option value="3">Montana</option>
-                    </select>
-                  </div>
-                </div>
-                <div>
-                  <label
-                    htmlFor="zipcode"
-                    className="block mb-2 text-sm font-medium text-gray-900 "
-                  >
-                    Zip Code
-                  </label>
+              <div className="flex align-bottom">
+                <label className="block mb-2 text-sm font-medium text-gray-900 ">
                   <input
-                    type="text"
-                    id="zipcode"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                    type="checkbox"
+                    className=" w-3 h-3 mr-1 border-gray-300 focus:ring-0 focus:ring-offset-0"
+                    {...register('address')}
                   />
-                </div>
-              </fieldset>
+                  <span>Address</span>
+                </label>
+              </div>
+
+              {address && (
+                <fieldset className="border border-solid border-gray-300 p-3">
+                  <div>
+                    <label
+                      htmlFor="street"
+                      className="block mb-2 text-sm font-medium text-gray-900 "
+                    >
+                      Street
+                    </label>
+                    <input
+                      type="text"
+                      id="street"
+                      className={`bg-gray-50 border  text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 focus:outline-none ${
+                        errors.street ? 'border-red-500' : 'border-gray-300'
+                      }`}
+                      {...register('street', {shouldUnregister: true})}
+                    />
+                    {/* {errors.street && (
+                      <span className="text-red-600 block mt-2 text-right">
+                        {errors.street?.message}
+                      </span>
+                    )} */}
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="city"
+                      className="block mb-2 text-sm font-medium text-gray-900 "
+                    >
+                      City
+                    </label>
+                    <input
+                      type="text"
+                      id="city"
+                      className={`bg-gray-50 border  text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 focus:outline-none ${
+                        errors.city ? 'border-red-500' : 'border-gray-300'
+                      }`}
+                      {...register('city', {shouldUnregister: true})}
+                    />
+                    {/* {errors.city && (
+                      <span className="text-red-600 block mt-2 text-right">
+                        {errors.city?.message}
+                      </span>
+                    )} */}
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="state"
+                      className="block mb-2 text-sm font-medium text-gray-900 "
+                    >
+                      State
+                    </label>
+
+                    <div className="mb-3 xl:w-96">
+                      <select
+                        id="state"
+                        className="form-select bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 "
+                        {...register('state', {shouldUnregister: true})}
+                      >
+                        <option value="Alabama">Alabama</option>
+                        <option value="Ohio">Ohio</option>
+                        <option value="Montana">Montana</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="zipcode"
+                      className="block mb-2 text-sm font-medium text-gray-900 "
+                    >
+                      Zip Code
+                    </label>
+                    <input
+                      type="text"
+                      id="zipcode"
+                      className={`bg-gray-50 border  text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 focus:outline-none ${
+                        errors.zipcode ? 'border-red-500' : 'border-gray-300'
+                      }`}
+                      {...register('zipcode', {shouldUnregister: true})}
+                    />
+                    {/* {errors.zipcode && (
+                      <span className="text-red-600 block mt-2 text-right">
+                        {errors.zipcode?.message}
+                      </span>
+                    )} */}
+                  </div>
+                </fieldset>
+              )}
               <div>
                 <label
                   htmlFor="department"
@@ -145,19 +260,23 @@ const Form = () => {
                   <select
                     id="state"
                     className="form-select bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 "
+                    {...register('department')}
                   >
-                    <option value="1">Sales</option>
-                    <option value="2">Marketing</option>
-                    <option value="3">Engineering</option>
-                    <option value="3">Human Ressources</option>
-                    <option value="3">Legal</option>
+                    <option value="Sales">Sales</option>
+                    <option value="Marketing">Marketing</option>
+                    <option value="Engineering">Engineering</option>
+                    <option value="Human Ressources">Human Ressources</option>
+                    <option value="Legal">Legal</option>
                   </select>
                 </div>
               </div>
 
               <button
                 type="submit"
-                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                className={`w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 ${
+                  isSubmitting && 'disabled'
+                }`}
+                disabled={isSubmitting}
               >
                 Save
               </button>
---
+++ src/types/employee.model.ts
@@ -0,0 +1,55 @@
+import {z} from 'zod'
+import isEmail from 'validator/lib/isEmail'
+
+const EmployeeWithoutAddressSchema = z.object({
+  firstname: z.string().trim().min(1, {message: 'First name is required'}),
+  lastname: z.string().trim().min(1, {message: 'Last name is required'}),
+  email: z.string().trim().min(1, {message: 'Email is required'}).email(),
+  birthdate: z.preprocess(arg => {
+    if (typeof arg == 'string' || arg instanceof Date) return new Date(arg)
+  }, z.date()),
+  startdate: z.preprocess(arg => {
+    if (typeof arg == 'string' || arg instanceof Date) return new Date(arg)
+  }, z.date()),
+  address: z.literal(false),
+  department: z.enum([
+    'Sales',
+    'Marketing',
+    'Engineering',
+    'Human Ressources',
+    'Legal',
+  ]),
+})
+
+const AddressSchema = z.object({
+  address: z.literal(true),
+  street: z
+    .string({
+      required_error: 'Street is required',
+    })
+    .trim()
+    .min(1, {message: 'Street is required'}),
+  city: z
+    .string({
+      required_error: 'City is required',
+    })
+    .trim()
+    .min(1, {message: 'City is required'}),
+  state: z.enum(['Alabama', 'Ohio', 'Montana']),
+  zipcode: z
+    .string({
+      required_error: 'Zipcode is required',
+    })
+    .trim()
+    .min(1, {message: 'Zipcode is required'}),
+})
+
+const EmployeeWithAddressSchema =
+  EmployeeWithoutAddressSchema.merge(AddressSchema)
+
+export const EmployeeSchema = z.discriminatedUnion('address', [
+  EmployeeWithoutAddressSchema,
+  EmployeeWithAddressSchema,
+])
+
+export type EmployeeSchemaType = z.infer<typeof EmployeeSchema>
