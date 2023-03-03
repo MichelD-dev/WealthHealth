ffacc4d8ff73a544f959e0e6deaf9eef4b78d604
michelD-dev <delaunaymichel@hotmail.fr>
2023-03-03 17:59:58 +0100

    Remplacement top level await pour compatibilité

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -20,25 +20,30 @@ interface AuthProviderProps {
   children: ReactNode
 }
 
-const {data} = await supabase.auth.getSession()
-
 const AuthProvider = ({children}: AuthProviderProps) => {
-  const [user, setUser] = useState<User | null>(data.session?.user as User)
+  const [user, setUser] = useState<User | null>(null)
 
   useEffect(() => {
-    const {data: authListener} = supabase.auth.onAuthStateChange(
-      (event, session) => {
-        if (event === 'SIGNED_IN') {
-          setUser(session?.user as User)
-        }
-        if (event === 'SIGNED_OUT') {
-          setUser(null)
-        }
-      },
-    )
-    return () => {
-      authListener.subscription.unsubscribe()
+    const fetchSession = async () => {
+      const {data} = await supabase.auth.getSession()
+
+      setUser(data.session?.user as User)
+
+      const {data: authListener} = supabase.auth.onAuthStateChange(
+        (event, session) => {
+          if (event === 'SIGNED_IN') {
+            setUser(session?.user as User)
+          }
+          if (event === 'SIGNED_OUT') {
+            setUser(null)
+          }
+        },
+      )
+      return () => {
+        authListener.subscription.unsubscribe()
+      }
     }
+    fetchSession()
   }, [])
 
   return <AuthContext.Provider value={{user}}>{children}</AuthContext.Provider>
