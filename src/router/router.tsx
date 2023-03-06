6fba4f9e3cc44301a76d4c096f26510f03d41c0b
michelD-dev <delaunaymichel@hotmail.fr>
2023-03-06 20:21:48 +0100

    Réécriture logique authentification

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,33 +1,60 @@
 import {
   createBrowserRouter,
   createRoutesFromElements,
+  Navigate,
   Route,
+  useLocation,
 } from 'react-router-dom'
 import {Suspense, lazy} from 'react'
 import App from '@/App'
 import Spinner from '@/components/Spinner/Spinner'
+import {useAuth} from '@/context/AuthProvider'
 
 const Form = lazy(() => import('@/pages/Employee_creation/Creation'))
+const LoginForm = lazy(() => import('@/pages/Login/Login'))
 const List = lazy(() => import('@/pages/Employee_list/List'))
 const Error404 = lazy(() => import('@/pages/error404/Error404'))
+
+function RequireAuth({children}: {children: JSX.Element}) {
+  const {user} = useAuth()
+  const location = useLocation()
+
+  if (user === null) {
+    return <Navigate to="/" state={{from: location}} replace />
+  }
+
+  return children
+}
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route
-        path="create"
+        index
         element={
           <Suspense fallback={<Spinner />}>
-            <Form />
+            <LoginForm />
           </Suspense>
         }
       />
       <Route
-        index
+        path="create"
         element={
-          <Suspense fallback={<Spinner />}>
-            <List />
-          </Suspense>
+          <RequireAuth>
+            <Suspense fallback={<Spinner />}>
+              <Form />
+            </Suspense>
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="list"
+        element={
+          <RequireAuth>
+            <Suspense fallback={<Spinner />}>
+              <List />
+            </Suspense>
+          </RequireAuth>
         }
       />
       <Route
